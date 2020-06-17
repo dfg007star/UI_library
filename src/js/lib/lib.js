56817de928aa44e6c99d2a566e0b792c02f0a6d3
Dima <54286444+dfg007star@users.noreply.github.com>
2020-06-17 19:17:35 +0300

    0.6

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -4,5 +4,6 @@ import './modules/classes';
 import './modules/handlers';
 import './modules/atribute';
 import './modules/actions';
+import './modules/effects';
 
 export default $;
--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,4 @@
 import './lib/lib';
 import $ from './lib/lib';
 
-$('button').on('click', function () {
-  $('div').eq(2).toggleClass('active');
-});
-
-$('input').setAtt('id', 'lol');
-$('input').setAtt('type', 'checkbox');
-$('input').setAtt('checked');
-console.log($('input').getAtt('id'));
-
-console.log($('button').html(''));
-
-$('div').click(function () {
-  console.log($(this).index());
-});
-
-// console.log($('div').eq(2).find('.some'));
-
-console.log($('.more').closest('.findme').addClass('closest')); //!!!!!
-
-console.log($('.more').eq(0).siblings());
+$('button').fadeOut(1800);
